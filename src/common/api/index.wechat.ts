61480bd5185bbb1749ae06e518124cdd62f8f1ff
Ling2048 <L2048@outlook.com>
2020-06-28 18:02:32 +0800

    refactor: 修改网络请求

--- src/common/api/index.wechat.ts
+++ src/common/api/index.wechat.ts
@@ -1,16 +1,13 @@
 import { request } from 'remax/wechat';
 const base = 'https://music.163.com';
 
-const requestA = (url: string) => {
+const requestWX = (url: string, init: {data: any, headers: any, method: string}) => {
     return request({
         url: base + url,
-        header: {
-            Accept: "*/*",
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: 'POST',
-        data: "params=k58%2FWRjVUBaC0an3ntpuK%2FqfIz0gXYNG%2FDc7uS74S3U%3D&encSecKey=7beee1b32f26bcea16e812035f6cd0e232bee8bb7d4ff844634e1041451785edbb9baf1402880c33b462d94b5853a09df3e3aebc9e2f851b2db850360cc69a546fd86fa7019fb3ec467e19e1660a4335f1c48aa3fb9833b892ce7d309e4cbafe50e640f4c4326a11ce35856b28501de36d9ce32a615ca44afc7d9b8e017a8f6e",
+        header: init.headers,
+        method: init.method as any,
+        data: init.data,
     }).then(res => res.data);
 }
 
-export {requestA as request}
+export {requestWX as request}
